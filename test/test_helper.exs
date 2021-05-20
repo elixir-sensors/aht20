48da93939b109b1576387984176419df1768affc
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-20 14:14:28 -0400

    Always waring as errors

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,8 @@
+# Always warning as errors
+if Version.match?(System.version(), "~> 1.10") do
+  Code.put_compiler_option(:warnings_as_errors, true)
+end
+
 Application.put_env(:aht20, :transport_module, AHT20.MockI2C)
 Mox.defmock(AHT20.MockI2C, for: AHT20.Transport)
 
