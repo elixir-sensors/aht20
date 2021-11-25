96d3e308cff559795ca64a04ba815f32dde9e675
Masatoshi N <nishiguchi.masa@gmail.com>
2021-11-25 15:52:05 -0500

    Implement Comm.get_status/1

--- lib/aht20/comm.ex
+++ lib/aht20/comm.ex
@@ -1,9 +1,33 @@
 defmodule AHT20.Comm do
   @moduledoc false
 
+  @aht20_cmd_status [0x71]
   @aht20_cmd_soft_reset [0xBA]
   @aht20_cmd_initialize [0xBE, <<0x08, 0x00>>]
   @aht20_cmd_trigger_measurement [0xAC, <<0x33, 0x00>>]
+
+  def get_status(transport) do
+    case AHT20.Transport.I2C.write_read(transport, @aht20_cmd_status, 1) do
+      {:ok, <<busy::1, _::3, calibrated::1, _::3>>} -> %{busy: busy, calibrated: calibrated}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def calibrated?(transport) do
+    case get_status(transport) do
+      %{calibrated: 1} -> true
+      %{calibrated: 0} -> false
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def busy?(transport) do
+    case get_status(transport) do
+      %{busy: 1} -> true
+      %{busy: 0} -> false
+      {:error, reason} -> {:error, reason}
+    end
+  end
 
   def reset(transport) do
     AHT20.Transport.I2C.write(transport, @aht20_cmd_soft_reset)
